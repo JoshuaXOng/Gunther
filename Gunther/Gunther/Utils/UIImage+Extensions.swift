cb1616877febd5c41b6efebe4316ef327a2fb5f1
JoshuaXOng <63457760+JoshuaXOng@users.noreply.github.com>
2021-05-01 17:48:35 +1000

    Changed resizeImage() extension of UIImage -- now has expected behavior.

--- Gunther/Gunther/Utils/UIImage+Extensions.swift
+++ Gunther/Gunther/Utils/UIImage+Extensions.swift
@@ -22,11 +22,11 @@ extension UIImage {
 
         // Figure out what our orientation is, and use that to form the rectangle
         var newSize: CGSize
-        if(widthRatio > heightRatio) {
-            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
-        } else {
-            newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
-        }
+        //if(widthRatio > heightRatio) {
+        //    newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        //} else {
+        newSize = CGSize(width: size.width * widthRatio,  height: size.height * heightRatio)
+        //}
 
         // This is the rect that we've calculated out and this is what is actually used below
         let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
