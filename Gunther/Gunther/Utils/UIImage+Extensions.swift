81b8ed260460566f0d52a581caa7bbcf8df56972
JoshuaXOng <63457760+JoshuaXOng@users.noreply.github.com>
2021-05-03 12:09:27 +1000

    In UIImage+Extension: - Added assertion to cropImage() - Removed comments in resizeImage()

--- Gunther/Gunther/Utils/UIImage+Extensions.swift
+++ Gunther/Gunther/Utils/UIImage+Extensions.swift
@@ -11,12 +11,17 @@ import UIKit
 extension UIImage {
     
     static func cropImage(image: UIImage, region: CGRect) -> UIImage? {
+        
         guard let cgImage = image.cgImage,
               let croppedCGImage = cgImage.cropping(to: region) else {
             return nil
         }
         let image = UIImage(cgImage: croppedCGImage)
+        
+        assert((image.size.width == region.width) && (image.size.height == region.height), "Extension method cropImage() implementation is incorrect.")
+        
         return image
+        
     }
     
     /* CODE FROM: https://stackoverflow.com/questions/31314412/how-to-resize-image-in-swift#:~:text=Image%20resize%20function%20in%20swift%20as%20below.&text=swift3%20updated-,func%20resizeImage(image%3A%20UIImage%2C%20targetSize%3A%20CGSize)%20%2D,width%20%2F%20size.
@@ -24,29 +29,21 @@ extension UIImage {
     */
     
     static func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+        
         let size = image.size
-
         let widthRatio  = targetSize.width  / size.width
         let heightRatio = targetSize.height / size.height
+        let newSize = CGSize(width: size.width * widthRatio,  height: size.height * heightRatio)
 
-        // Figure out what our orientation is, and use that to form the rectangle
-        var newSize: CGSize
-        //if(widthRatio > heightRatio) {
-        //    newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
-        //} else {
-        newSize = CGSize(width: size.width * widthRatio,  height: size.height * heightRatio)
-        //}
-
-        // This is the rect that we've calculated out and this is what is actually used below
         let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
 
-        // Actually do the resizing to the rect using the ImageContext stuff
         UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
         image.draw(in: rect)
         let newImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
 
         return newImage!
+        
     }
     
 }
