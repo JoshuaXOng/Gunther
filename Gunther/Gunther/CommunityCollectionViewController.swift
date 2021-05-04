3904e84d42af811d708618cdd4b8f0f5a9e6776f
Joshua Ong <joshuaong2000@gmail.com>
2021-05-04 13:40:42 +1000

    Added CommunityCollectionViewController.

---
+++ Gunther/Gunther/CommunityCollectionViewController.swift
@@ -0,0 +1,94 @@
+//
+//  CommunityCollectionViewController.swift
+//  Gunther
+//
+//  Created by Joshua Ong on 4/5/21.
+//
+
+import UIKit
+
+class CommunityCollectionViewController: UICollectionViewController {
+    
+    var databaseController: DatabaseProtocol?
+    var listenerType = ListenerType.user
+    
+    let COMMUNITY_SECTION = 0
+    let COMMUNITY_CELL = "CommunityCell"
+    var categories = [Category]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            
+        // Get reference to database contoller
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return
+        }
+        databaseController = appDelegate.databaseController
+        
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: COMMUNITY_CELL)
+
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    
+        // Configure the cell
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
