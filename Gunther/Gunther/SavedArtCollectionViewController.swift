373871ffa574c9185f0445645b873b45ae7f5b52
JoshuaXOng <63457760+JoshuaXOng@users.noreply.github.com>
2021-05-08 17:53:22 +1000

    Updated SavedArtViewController

--- Gunther/Gunther/SavedArtCollectionViewController.swift
+++ Gunther/Gunther/SavedArtCollectionViewController.swift
@@ -12,11 +12,13 @@ import FirebaseFirestoreSwift
 class SavedArtCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout, DatabaseListener {
     
     var databaseController: DatabaseProtocol?
+    var firebaseController: FirebaseController?
     var listenerType = ListenerType.user
     
     let SAVED_ART_SECTION = 0
     let SAVED_ART_CELL = "SavedArtCell"
     var savedArt = [SavedArt]()
+    var savedArtImages = [UIImage?]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,14 +27,48 @@ class SavedArtCollectionViewController: UICollectionViewController, UICollection
         //self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: SAVED_ART_CELL)
         self.collectionView!.register(SavedArtCollectionViewCell.self, forCellWithReuseIdentifier: SAVED_ART_CELL)
         
-        // Get a reference to the applications database controller
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return
+        // Get a reference to the applications database controller (cast it into Firebase controller)
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
         }
         databaseController = appDelegate.databaseController
+        firebaseController = databaseController as? FirebaseController
+        
+        //fetchImages()
+        
+    }
+    
+    private func fetchImages() {
+        savedArtImages = [UIImage?](repeating: nil, count: savedArt.count)
+        var counter = 0
+        for (index, savedArtSingular) in savedArt.enumerated() {
+            
+            guard let source = savedArtSingular.source else { return }
+            
+            firebaseController?.fetchDataAtStorageRef(source: source) { data in
+                guard let savedArtUIImage = UIImage(data: data) else { return }
+                
+                self.savedArtImages.remove(at: index)
+                self.savedArtImages.insert(savedArtUIImage, at: index)
+                
+                counter += 1
+                //append(savedArtUIImage) // Can't append must replace -- we do not know the order in which responses will be delivered.
+                //if self.savedArtImages.count == self.savedArt.count {
+                    //onSavedArtImagesLoaded
+                if counter == self.savedArt.count {
+                    self.collectionView.reloadSections([self.SAVED_ART_SECTION])
+                }
+                //}
+            }
+            
+        }
+        
+        
         
     }
     
     // MARK: - Implement DatabaseListner
+    // With the flashing problem -- solution is likely, always listen, or prevent tab controller from caching.
 
     func onCategoriesChange(change: DatabaseChange, categories: [Category]) {}
     
@@ -40,19 +76,29 @@ class SavedArtCollectionViewController: UICollectionViewController, UICollection
         // Iterate through old art, if differnt to new, reload.
         savedArt = user.artworks
         // Try .reloadItems() or .reloadData()
-        collectionView.reloadSections([SAVED_ART_SECTION])
+        fetchImages()
+        //collectionView.reloadSections([SAVED_ART_SECTION])
     }
     
     // MARK: - View (dis)appearance setup/deconstructing
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        //savedArt = [SavedArt]()
         databaseController?.addListener(listener: self)
+        //fetchImages()
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         databaseController?.removeListener(listener: self)
+        //let savedArtSection = IndexSet(integer: SAVED_ART_SECTION)
+        //collectionView.deleteSections(savedArtSection)
+        for i in 0 ..< savedArt.count {
+            let ip = IndexPath(item: i, section: SAVED_ART_SECTION)
+            collectionView.deleteItems(at: [ip])
+        }
+        collectionView.reloadSections([SAVED_ART_SECTION])
     }
     
     // MARK: UICollectionViewDataSource
@@ -69,41 +115,36 @@ class SavedArtCollectionViewController: UICollectionViewController, UICollection
         
         let savedArtCell = collectionView.dequeueReusableCell(withReuseIdentifier: SAVED_ART_CELL, for: indexPath)
         
+        
+        
+        /*
         let savedArtSingular = savedArt[indexPath.row]
-        guard let source = savedArtSingular.source,
-              let firebaseController = databaseController as? FirebaseController else {
+        guard let source = savedArtSingular.source else {
             return savedArtCell
         }
-        // Makes sure to encode the URL.
-        let reference = firebaseController.storage.reference(withPath: source)
         
-        reference.getData(maxSize: 1 * 1024 * 1024) { data, error in
-            
-            if let error = error {
-                print(error)
+        firebaseController?.fetchDataAtStorageRef(source: source) { data in
+            guard let savedArtUIImage = UIImage(data: data) else { return }
+            let savedArtImageView = UIImageView(image: savedArtUIImage)
+            DispatchQueue.main.async {
+                savedArtCell.contentView.subviews.forEach({ $0.removeFromSuperview() })
+                savedArtCell.contentView.addSubview(savedArtImageView)
+                savedArtImageView.frame = savedArtCell.contentView.bounds
             }
-            
-            else {
-                
-                guard let imageOfSavedArt = UIImage(data: data!) else { return }
-                let imageViewOfSavedArt = UIImageView(image: imageOfSavedArt)
-                
-                // You need to identify which piece has changed and do the below just for that one.
-                DispatchQueue.main.async {
-                    savedArtCell.contentView.subviews.forEach({ $0.removeFromSuperview() })
-                    savedArtCell.contentView.addSubview(imageViewOfSavedArt)
-                    imageViewOfSavedArt.frame = savedArtCell.contentView.bounds
-                    //self.collectionView.reloadSections([self.SAVED_ART_SECTION])
-                }
-                
-            }
-            
         }
-    
+        */
+        
+        if savedArtImages.count == savedArt.count {
+            savedArtCell.contentView.subviews.forEach({ $0.removeFromSuperview() })
+            let savedArtImageView = UIImageView(image: savedArtImages[indexPath.row])
+            savedArtCell.contentView.addSubview(savedArtImageView)
+            savedArtImageView.frame = savedArtCell.contentView.bounds
+        }
+        
         return savedArtCell
         
     }
-    
+        
     // MARK: - Implement UICollectionViewDelegateFlowLayout
     
     /* CODE FROM:
