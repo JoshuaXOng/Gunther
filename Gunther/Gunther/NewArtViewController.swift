4b99f0418ae1d79088b4830fafbc48037ae7acb5
JoshuaXOng <63457760+JoshuaXOng@users.noreply.github.com>
2021-05-09 21:13:11 +1000

    Updated NewArtViewController

--- Gunther/Gunther/NewArtViewController.swift
+++ Gunther/Gunther/NewArtViewController.swift
@@ -20,18 +20,43 @@ class NewArtViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var pixelSizeSlider: UISlider!
     @IBOutlet weak var pixelSize: UILabel!
     
+    @IBOutlet weak var fromCameraButton: UIButton!
+    @IBOutlet weak var fromBlankCanvasButton: UIButton!
+    let buttons = [fromCameraButton, fromBlankCanvasButton]
+    
     @IBAction func widthSlider(_ sender: UISlider) {
         let roundedWidth = widthSlider.value - widthSlider.value.truncatingRemainder(dividingBy: 5)
-        widthLabel.text = String(roundedWidth)
+        widthLabel.text = String(Int(roundedWidth))
     }
     @IBAction func heightSlider(_ sender: UISlider) {
         let roundedHeight = heightSlider.value - heightSlider.value.truncatingRemainder(dividingBy: 5)
-        heightLabel.text = String(roundedHeight)
+        heightLabel.text = String(Int(roundedHeight))
     }
     @IBAction func pixelSizeSlider(_ sender: UISlider) {
         let roundedPixelSize = pixelSizeSlider.value - pixelSizeSlider.value.truncatingRemainder(dividingBy: 1)
-        pixelSize.text = String(roundedPixelSize)
+        pixelSize.text = String(Int(roundedPixelSize))
     }
+    
+    @IBAction func fromCameraButton(_ sender: UIButton) {
+        updateButtonsRadiolike(buttons: buttons, choice: sender)
+    }
+    
+    @IBAction func fromBlankCanvasButton(_ sender: UIButton) {
+        updateButtonsRadiolike(buttons: buttons, choice: sender)
+    }
+    
+    @IBAction func startBarButton(_ sender: UIBarButtonItem) {
+        let isInputValid = !(artNameField.text?.trimmingCharacters(in: .whitespaces) == "")
+        if isInputValid {
+            performSegue(withIdentifier: "NewArtBCToArtSegue", sender: nil)
+        }
+        else {
+            displayMessage(title: "Invalid Input", message: "Please enter a valid name.")
+        }
+        resetInputs()
+    }
+    
+    // MARK: - viewDidLoad()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,10 +67,22 @@ class NewArtViewController: UIViewController, UITextFieldDelegate {
         
         artNameField.delegate = self
         
-        widthLabel.text = String(widthSlider.value)
-        heightLabel.text = String(heightSlider.value)
-        pixelSize.text = String(pixelSizeSlider.value)
+        widthLabel.text = String(Int(widthSlider.value))
+        heightLabel.text = String(Int(heightSlider.value))
+        pixelSize.text = String(Int(pixelSizeSlider.value))
         
+    }
+    
+    // MARK: - Util for adding radio button like behaviour to a group of UIButtons
+    
+    private func updateButtonsRadiolike(buttons: [UIButton], choice: UIButton) {
+        
+    }
+    
+    // MARK: - Util for clearing inputs upon view transition
+    
+    private func resetInputs() {
+        self.artNameField.text = ""
     }
     
     // MARK: - Functions for resizing width, height and pixel size
