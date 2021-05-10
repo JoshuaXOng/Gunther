186b254ed65a77407f0e20b6cd5405cd8a9f1974
JoshuaXOng <63457760+JoshuaXOng@users.noreply.github.com>
2021-05-10 17:03:54 +1000

    Updated ToolPickerViewController

--- Gunther/Gunther/ToolPickerViewController.swift
+++ Gunther/Gunther/ToolPickerViewController.swift
@@ -7,12 +7,39 @@
 
 import UIKit
 
-class ToolPickerViewController: UIViewController {
+class ToolPickerViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var typeCollectionView: UICollectionView!
+    @IBOutlet weak var sizeCollectionView: UICollectionView!
+    let TYPE_CELL = "TypeCell"
+    let SIZE_CELL = "SizeCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        typeCollectionView.delegate = self
+        sizeCollectionView.delegate = self
+        typeCollectionView.dataSource = self
+        sizeCollectionView.dataSource = self
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == typeCollectionView {
+            return 2
+        }
+        else {
+            return 8
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if collectionView == typeCollectionView {
+            let typeCell = collectionView.dequeueReusableCell(withReuseIdentifier: TYPE_CELL, for: indexPath)
+            return typeCell
+        }
+        let sizeCell = collectionView.dequeueReusableCell(withReuseIdentifier: SIZE_CELL, for: indexPath)
+        return sizeCell
     }
     
     /*
