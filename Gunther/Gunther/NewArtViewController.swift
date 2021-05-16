9ed060c44e6c842821615f5deb6649b3e1fdcf53
JoshuaXOng <63457760+JoshuaXOng@users.noreply.github.com>
2021-05-16 15:27:55 +1000

    Updated appearance of 'new art' screen: - Updated storyboard - Updated NewArtViewController

--- Gunther/Gunther/NewArtViewController.swift
+++ Gunther/Gunther/NewArtViewController.swift
@@ -11,8 +11,10 @@ class NewArtViewController: UIViewController, UITextFieldDelegate {
 
     private var databaseController: DatabaseProtocol?
     
+    // Name of Art field.
     @IBOutlet weak var artNameField: UITextField!
     
+    // Sliders and corresponding numbers.
     @IBOutlet weak var widthSlider: UISlider!
     @IBOutlet weak var widthLabel: UILabel!
     @IBOutlet weak var heightSlider: UISlider!
@@ -20,11 +22,14 @@ class NewArtViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var pixelSizeSlider: UISlider!
     @IBOutlet weak var pixelSize: UILabel!
     
+    // Start from buttons.
     @IBOutlet weak var fromCameraButton: UIButton!
     @IBOutlet weak var fromBlankCanvasButton: UIButton!
     let START_FROM_CAMERA = "startFromCamera"
     let START_FROM_BLANK_CANVAS = "startFromBlankCanvas"
     var startFromChoice: String?
+    
+    // MARK: - Sliders
     
     @IBAction func widthSlider(_ sender: UISlider) {
         let roundedWidth = widthSlider.value - widthSlider.value.truncatingRemainder(dividingBy: 5)
@@ -38,6 +43,8 @@ class NewArtViewController: UIViewController, UITextFieldDelegate {
         let roundedPixelSize = pixelSizeSlider.value - pixelSizeSlider.value.truncatingRemainder(dividingBy: 1)
         pixelSize.text = String(Int(roundedPixelSize))
     }
+    
+    // MARK: - Buttons
     
     @IBAction func fromCameraButton(_ sender: UIButton) {
         updateButtonsRadiolike(buttons: [fromCameraButton, fromBlankCanvasButton], choice: fromCameraButton)
@@ -67,37 +74,6 @@ class NewArtViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
-    // MARK: - Util for validating form input
-    
-    private func validateInput() -> Bool {
-        let isArtNameValid = !(artNameField.text?.trimmingCharacters(in: .whitespaces) == "")
-        let isStartFromChoiceValid = startFromChoice
-        if isArtNameValid && isStartFromChoiceValid != nil {
-            return true
-        }
-        return false
-    }
-    
-    // MARK: - Utils for adding radio-like behaviour to UIButtons
-    
-    private func applyToggleAppearance(button: UIButton) {
-        button.layer.shadowColor = UIColor.black.cgColor
-        button.layer.shadowOffset = CGSize(width: 1, height: 1)
-        button.layer.shadowRadius = 4
-        button.layer.shadowOpacity = 1
-    }
-    
-    private func applyUntoggleAppearance(button: UIButton) {
-        button.layer.shadowOpacity = 0
-    }
-    
-    private func updateButtonsRadiolike(buttons: [UIButton], choice: UIButton) {
-        for button in buttons {
-            applyUntoggleAppearance(button: button)
-        }
-        applyToggleAppearance(button: choice)
-    }
-    
     // MARK: - viewDidLoad()
     
     override func viewDidLoad() {
@@ -109,10 +85,80 @@ class NewArtViewController: UIViewController, UITextFieldDelegate {
         
         artNameField.delegate = self
         
+        setupSliders()
+        
+        setupStartFromButton(button: fromCameraButton)
+        setupStartFromButton(button: fromBlankCanvasButton)
+        
+    }
+    
+    // MARK: - Utils for views setup
+    
+    private func setupSliders() {
+        
+        widthSlider.minimumTrackTintColor = UIColor(red: 0.79, green: 0.83, blue: 0.89, alpha: 1)
+        widthSlider.thumbTintColor = UIColor(red: 0.48, green: 0.72, blue: 0.51, alpha: 1)
+        
+        heightSlider.minimumTrackTintColor = UIColor(red: 0.79, green: 0.83, blue: 0.89, alpha: 1)
+        heightSlider.thumbTintColor = UIColor(red: 0.48, green: 0.72, blue: 0.51, alpha: 1)
+        
+        pixelSizeSlider.minimumTrackTintColor = UIColor(red: 0.79, green: 0.83, blue: 0.89, alpha: 1)
+        pixelSizeSlider.thumbTintColor = UIColor(red: 0.48, green: 0.72, blue: 0.51, alpha: 1)
+        
         widthLabel.text = String(Int(widthSlider.value))
         heightLabel.text = String(Int(heightSlider.value))
         pixelSize.text = String(Int(pixelSizeSlider.value))
+    
+    }
+    
+    private func setupStartFromButton(button: UIButton) {
         
+        button.layer.cornerRadius = 2.5
+        button.backgroundColor = UIColor(red: 0.48, green: 0.72, blue: 0.51, alpha: 1)
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOffset = CGSize(width: 0.5, height: 1)
+        button.layer.shadowRadius = 2
+        button.layer.shadowOpacity = 0.5
+        
+    }
+    
+    // MARK: - Radio-like behaviour to UIButtons
+    
+    private func applyToggleAppearance(button: UIButton) {
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOffset = CGSize(width: 0.5, height: 1)
+        button.layer.shadowRadius = 2
+        button.layer.shadowOpacity = 1
+    }
+    
+    private func applyUntoggleAppearance(button: UIButton) {
+        setupStartFromButton(button: button)
+    }
+    
+    private func updateButtonsRadiolike(buttons: [UIButton], choice: UIButton) {
+        for button in buttons {
+            applyUntoggleAppearance(button: button)
+        }
+        applyToggleAppearance(button: choice)
+    }
+    
+    // MARK: - Util validating form input
+    
+    private func validateInput() -> Bool {
+        let isArtNameValid = !(artNameField.text?.trimmingCharacters(in: .whitespaces) == "")
+        let isStartFromChoiceValid = startFromChoice
+        if isArtNameValid && isStartFromChoiceValid != nil {
+            return true
+        }
+        return false
+    }
+    
+    // MARK: - Util for resizing width, height and pixel size
+    
+    private func roundDimensionsForPixelSize(width: Float, height: Float, pixelSize: Float) -> (Float, Float) {
+        let roundedWidth = width - width.truncatingRemainder(dividingBy: pixelSize)
+        let roundedHeight = height - height.truncatingRemainder(dividingBy: pixelSize)
+        return (roundedWidth, roundedHeight)
     }
     
     // MARK: - Util for clearing inputs upon view transition
@@ -125,15 +171,7 @@ class NewArtViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
-    // MARK: - Functions for resizing width, height and pixel size
-    
-    private func roundDimensionsForPixelSize(width: Float, height: Float, pixelSize: Float) -> (Float, Float) {
-        let roundedWidth = width - width.truncatingRemainder(dividingBy: pixelSize)
-        let roundedHeight = height - height.truncatingRemainder(dividingBy: pixelSize)
-        return (roundedWidth, roundedHeight)
-    }
-    
-    // MARK: - Implement UITextFieldDelegate
+    // MARK: - UITextFieldDelegate
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
