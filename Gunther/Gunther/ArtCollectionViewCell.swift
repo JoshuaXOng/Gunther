dff96b7b9584dbc6e5fe6575edd60301d1c114ba
JoshuaXOng <63457760+JoshuaXOng@users.noreply.github.com>
2021-05-23 17:20:52 +1000

    Refactored ArtCollectionViewCell.

--- Gunther/Gunther/ArtCollectionViewCell.swift
+++ Gunther/Gunther/ArtCollectionViewCell.swift
@@ -18,24 +18,44 @@ class ArtCollectionViewCell: UICollectionViewCell {
         self.contentView.backgroundColor = UIColor(red: 0.79, green: 0.83, blue: 0.89, alpha: 1)
         self.contentView.layer.cornerRadius = 5
         
-        imageView = UIImageView(frame: CGRect(x: 15, y: 15, width: self.bounds.size.width-30, height: self.bounds.size.height-30))
-        imageView!.contentMode = UIView.ContentMode.scaleAspectFit
-        imageView!.layer.shadowColor = UIColor.black.cgColor
-        imageView!.layer.shadowOffset = CGSize(width: 0.5, height: 1)
-        imageView!.layer.shadowRadius = 2
-        imageView!.layer.shadowOpacity = 0.5
-        self.contentView.addSubview(imageView!)
-        
-        label = UILabel(frame: CGRect(x: 0, y: self.bounds.size.height-8, width: self.bounds.size.width, height: 40))
-        self.contentView.addSubview(label!)
-        label!.translatesAutoresizingMaskIntoConstraints = false
-        label!.centerXAnchor.constraint(equalTo: contentView.centerXAnchor, constant: 0).isActive = true
-        label!.topAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 3).isActive = true
+        setupLabel()
+        setupImageView()
         
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupLabel() {
+        
+        label = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: 20))
+        contentView.addSubview(label!)
+        
+        label!.translatesAutoresizingMaskIntoConstraints = false
+        label!.centerXAnchor.constraint(equalTo: contentView.centerXAnchor, constant: 0).isActive = true
+        label!.topAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 3).isActive = true
+    
+    }
+    
+    private func setupImageView() {
+        
+        imageView = UIImageView(frame: CGRect(x: 15, y: 15, width: contentView.bounds.width-30, height: contentView.bounds.height-30))
+        imageView!.contentMode = UIView.ContentMode.scaleAspectFit
+        imageView!.layer.shadowColor = UIColor.black.cgColor
+        imageView!.layer.shadowOffset = CGSize(width: 0.5, height: 1)
+        imageView!.layer.shadowRadius = 2
+        imageView!.layer.shadowOpacity = 0.5
+        contentView.addSubview(imageView!)
+        
+        /*
+        imageView!.translatesAutoresizingMaskIntoConstraints = false
+        imageView!.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15).isActive = true
+        imageView!.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -15).isActive = true
+        imageView!.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 15).isActive = true
+        imageView!.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -15).isActive = true
+        */
+        
     }
     
 }
