c827a7024135ce24fadba2c8373053375042f092
JoshuaXOng <63457760+JoshuaXOng@users.noreply.github.com>
2021-05-23 17:24:43 +1000

    Refactored CommunityCollectionViewCell.

--- Gunther/Gunther/CommunityCollectionViewCell.swift
+++ Gunther/Gunther/CommunityCollectionViewCell.swift
@@ -11,7 +11,6 @@ class CommunityCollectionViewCell: UICollectionViewCell {
     
     var label: UILabel?
     var imageView: UIImageView?
-    var reused = false
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -20,18 +19,33 @@ class CommunityCollectionViewCell: UICollectionViewCell {
         layer.borderColor = UIColor(red: 0.79, green: 0.83, blue: 0.89, alpha: 1).cgColor
         
         contentView.layer.cornerRadius = 2.5
-        //contentView.layer.masksToBounds = true
-        /*contentView.layer.shadowColor = UIColor.black.cgColor
-        contentView.layer.shadowOffset = CGSize(width: 0.5, height: 1)
-        contentView.layer.shadowRadius = 2
-        contentView.layer.shadowOpacity = 0.5*/
+        
+        setupLabel()
+        setupImageView()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupLabel() {
+        
+        label = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: 20))
+        addSubview(label!)
+    
+        label!.translatesAutoresizingMaskIntoConstraints = false
+        label!.centerXAnchor.constraint(equalTo: centerXAnchor, constant: 0).isActive = true
+        label!.topAnchor.constraint(equalTo: bottomAnchor, constant: 3).isActive = true
+        label!.text = "Test :]"
+        
+    }
+    
+    private func setupImageView() {
         
         // Setup imageView.
-        imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: self.bounds.size.height))
+        imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: contentView.bounds.width, height: contentView.bounds.height))
         imageView!.contentMode = UIView.ContentMode.scaleToFill
-        if !reused {
-            contentView.addSubview(imageView!)
-        }
         
         imageView?.layer.shadowColor = UIColor.black.cgColor
         imageView?.layer.shadowOffset = CGSize(width: 0.5, height: 1)
@@ -39,22 +53,16 @@ class CommunityCollectionViewCell: UICollectionViewCell {
         imageView?.layer.shadowOpacity = 0.5
         imageView?.layer.masksToBounds = false
         
-        // Setup label.
-        label = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: 20))
-        if !reused {
-            addSubview(label!)
-        }
-        label!.translatesAutoresizingMaskIntoConstraints = false
-        label!.centerXAnchor.constraint(equalTo: centerXAnchor, constant: 3).isActive = true
-        label!.topAnchor.constraint(equalTo: bottomAnchor, constant: 3).isActive = true
-        label?.text = "TEST"
+        contentView.addSubview(imageView!)
         
-        reused = true
-        
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        /*
+        imageView!.translatesAutoresizingMaskIntoConstraints = false
+        imageView!.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        imageView!.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        imageView!.leftAnchor.constraint(equalTo: contentView.leftAnchor).isActive = true
+        imageView!.rightAnchor.constraint(equalTo: contentView.rightAnchor).isActive = true
+        */
+ 
     }
     
 }
