12e069266ff838e062e9af9224910d399094e83f
JoshuaXOng <63457760+JoshuaXOng@users.noreply.github.com>
2021-06-09 12:32:18 +1000

    Added documentation to DatabaseProtocol.

--- Gunther/Gunther/Database/DatabaseProtocol.swift
+++ Gunther/Gunther/Database/DatabaseProtocol.swift
@@ -14,20 +14,34 @@ enum DatabaseChange {
     case update
 }
 
+/*
+ * Contains the available types of database listeners.
+ * The value within this enum corresponds to what the
+ * database listener should 'listen' to.
+ */
 enum ListenerType {
     case categories
     case user
     case all
 }
 
+/*
+ * An interface for a database listener.
+ * A database listener is simply a class that responds to changes in
+ * the database.
+ */
 protocol DatabaseListener: AnyObject {
     var listenerType: ListenerType {get set}
     func onCategoriesChange(change: DatabaseChange, categories: [Category])
     func onUserChange(change: DatabaseChange, user: User)
 }
 
+/*
+ * An interface for a database controller.
+ */
 protocol DatabaseProtocol: AnyObject {
     
+    // To persistently save modifications to data in the database.
     func cleanup()
     
     func addListener(listener: DatabaseListener)
