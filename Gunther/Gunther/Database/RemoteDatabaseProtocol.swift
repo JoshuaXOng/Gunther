402819dab68e31696d03d782a15d8e567f657b1f
JoshuaXOng <63457760+JoshuaXOng@users.noreply.github.com>
2021-06-09 12:37:12 +1000

    Added documentation to RemoteDatabaseProtocol.

--- Gunther/Gunther/Database/RemoteDatabaseProtocol.swift
+++ Gunther/Gunther/Database/RemoteDatabaseProtocol.swift
@@ -8,6 +8,13 @@
 import Foundation
 import UIKit
 
+/*
+ * Inherits from DatabaseProtocol.
+ * This is a more specific database interface -- it is to be used for
+ * databases that are 'remote', i.e. not within the device.
+ * This interface mainly includes callbacks, which are quite necessary
+ * for remote databases and not so much for something like CoreData.
+ */
 protocol RemoteDatabaseProtocol: DatabaseProtocol {
     
     func fetchArtImageFromArt(art: SavedArt, completionHandler: @escaping (UIImage?) -> Void) throws
